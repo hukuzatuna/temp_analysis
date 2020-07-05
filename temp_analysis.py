1604819bdd33b7436f9b2978249133c5fd9b8d8d
Phil Moyer <phil@moyer.ai>
2020-07-04 22:23:54 -0700

    Fixed stats generation by column

--- temp_analysis.py
+++ temp_analysis.py
@@ -21,15 +21,10 @@ roomtemp_pi.py program.
 # Standard libraries modules
 
 import statistics
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from numpy.polynomial.polynomial import polyfit
-import sklearn
-import hashlib
-from sklearn.model_selection import train_test_split
 
 
 # Third-party modules
@@ -41,7 +36,8 @@ from sklearn.model_selection import train_test_split
 # Globals
 ######################
 
-data_path = "./TempData.csv"
+# data_path = "/home/pi/src/roomtemp/roomtemp_pi/output/data_in.csv"
+data_path = "./data_in.csv"
 output_path = "./images/"
 
 color_data = {
@@ -76,29 +72,22 @@ color_data = {
 # Functions
 ######################
 
-def test_set_check(identifier, test_ratio, hash):
-    return(hash(np.int64(identifier)).digets()[-1] < 256 * test_ratio)
-
-
-def split_train_test_by_id(data, test_ratio, id_column, has=hashlib.md5):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
-    return(data.loc[~in_test_set], data.loc[in_test_set])
-
-
 def main():
+    """Abstract main() into a function. Normally exits after execution.
+
+    A function abstracting the main code in the module, which
+    allows it to be used for libraries as well as testing (i.e., it can be
+    called as a script for testing or imported as a library, without
+    modification).
+    """
 
     # Read the data into a matrix
     # Col_Names = ["bme280","dps310","pct2075","hts221","mcp9808","sht31d",
     #     "lps35hw","lps22","htu21","si7021","lps25","mean"]
     Col_Names = ["dps310","pct2075","hts221","mcp9808","lps35hw","sht31d",
-            "si7021","htu21d","shtc3","aht20","mean","pressure","rh"]
+            "si7021","htu21d","shtc3","aht20","mean","press","rh"]
     tempDF = pd.read_csv(data_path, names=Col_Names)
 
-    # Add an index column
-    # DataDF = tempDF.reset_index()
-    
-    # Remove sensors with known-bad data
     DataDF = tempDF.drop("lps35hw",1)
 
     #----------------------------------------------
@@ -117,7 +106,7 @@ def main():
     ax.set_title('Temperature Sensor Time Series Comparison')
 
     for sensor in DataDF.keys():
-        if "pressure" != sensor and "rh" != sensor:
+        if "press" != sensor and "rh" != sensor:
             ax.plot(x, DataDF[sensor], c=color_data[sensor], ls='-', label=sensor, linewidth=0.5)
 
     plt.legend()
@@ -141,7 +130,7 @@ def main():
 
     DF2 = pd.DataFrame()
     for sensor in DataDF.keys():
-        if "pressure" != sensor and "rh" != sensor:
+        if "press" != sensor and "rh" != sensor:
             DF2[sensor] = DataDF[sensor] - DataDF['mean']
 
     # print(DF2.head(10))
@@ -158,7 +147,7 @@ def main():
     ax.set_title('Sensor Variance Around The Mean')
 
     for sensor in DF2.keys():
-        if "pressure" != sensor and "rh" != sensor:
+        if "press" != sensor and "rh" != sensor:
             ax.plot(x, DF2[sensor], c=color_data[sensor], ls='-', label=sensor, linewidth=0.5)
 
     plt.legend()
@@ -170,30 +159,29 @@ def main():
     # Statistics of the variance
     #----------------------------------------------
 
-    print("\nSensor Variance Around The Mean\n")
-    print("Sensor\tMax\tMin\tMean\tMedian")
+    print("\nSensor Variance Around The Mean:")
+    print("\tSensor\tMax\tMin\tMean\tMedian")
 
     for sensor in DF2.keys():
-        if "pressure" != sensor and "rh" != sensor:
-            t_var_max = DF2[sensor].max()
-            t_var_min = DF2[sensor].min()
-            t_var_mean = DF2[sensor].mean()
-            t_var_median = DF2[sensor].median()
-            print("%s\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % (sensor,
-                t_var_max,
-                t_var_min,
-                t_var_mean,
-                t_var_median))
+        t_var_max = DF2[sensor].max()
+        t_var_min = DF2[sensor].min()
+        t_var_mean = DF2[sensor].mean()
+        t_var_median = DF2[sensor].median()
+        print("\t%s\t%0.4f\t%0.4f\t%0.4f\t%0.4f" % (sensor,
+            t_var_max,
+            t_var_min,
+            t_var_mean,
+            t_var_median))
 
     print("")
 
 
     # Histograms of the variance
 
-    # out_file = "%s/variance_hist.jpg" % output_path
-    # plt.style.use('dark_background')
-    # hist = DF2.hist(bins=20, figsize=(12,5), layout=(3,5))
-    # plt.savefig(out_file)
+    out_file = "%s/variance_hist.jpg" % output_path
+    plt.style.use('dark_background')
+    hist = DF2.hist(bins=20, figsize=(12,5), layout=(3,5))
+    plt.savefig(out_file)
 
     # Variance by temperature
     
@@ -207,7 +195,7 @@ def main():
     ax.set_title('Sensor Variance by Temperature')
 
     for sensor in DataDF.keys():
-        if "pressure" != sensor and "rh" != sensor:
+        if "press" != sensor and "rh" != sensor:
             ax.scatter(DataDF[sensor], DF2[sensor], c=color_data[sensor], s=0.1,
                 label='_nolegend_')
             b,m = polyfit(DataDF[sensor], DF2[sensor], 1)
@@ -217,85 +205,6 @@ def main():
     plt.legend()
     out_file = "%s/VarxTemp.jpg" % output_path
     plt.savefig(out_file)
-
-    # Temperature by pressure
-    
-    plt.style.use('dark_background')
-    fig = plt.figure()
-    fig.set_size_inches(12,5)
-    fig.dpi = 300
-    ax = fig.add_subplot(111)
-    ax.set_ylabel('Temperature')
-    ax.set_xlabel('Pressure')
-    ax.set_title('Sensor Dependency on Pressure')
-
-    for sensor in DataDF.keys():
-        if "pressure" != sensor and "rh" != sensor:
-            ax.scatter(DataDF["pressure"], DataDF[sensor], c=color_data[sensor], s=0.1,
-                label='_nolegend_')
-            b,m = polyfit(DataDF["pressure"], DataDF[sensor], 1)
-            plt.plot(DataDF["pressure"], b + m * DataDF[sensor], '-', linewidth=0.4,
-                c=color_data[sensor], label=sensor)
-
-    plt.legend()
-    out_file = "%s/TempxPress.jpg" % output_path
-    plt.savefig(out_file)
-
-    # Temperature by humidity
-    
-    plt.style.use('dark_background')
-    fig = plt.figure()
-    fig.set_size_inches(12,5)
-    fig.dpi = 300
-    ax = fig.add_subplot(111)
-    ax.set_ylabel('Temperature')
-    ax.set_xlabel('Humidity')
-    ax.set_title('Sensor Dependency on Humidity')
-
-    for sensor in DataDF.keys():
-        if "pressure" != sensor and "rh" != sensor:
-            ax.scatter(DataDF["rh"], DataDF[sensor], c=color_data[sensor], s=0.1,
-                label='_nolegend_')
-            b,m = polyfit(DataDF["rh"], DataDF[sensor], 1)
-            plt.plot(DataDF["rh"], b + m * DataDF[sensor], '-', linewidth=0.4,
-                c=color_data[sensor], label=sensor)
-
-    plt.legend()
-    out_file = "%s/TempxRH.jpg" % output_path
-    plt.savefig(out_file)
-
-
-    #################################################
-    # 
-    # Machine Learning Time
-    #
-    #################################################
-
-    # Set aside a test set. Really want to use leave-one-out k-fold validation,
-    # but we'll get to that.
-
-    train_set, test_set = train_test_split(DataDF, test_size=0.2)
-
-    # Correlation
-
-    print("Correlation Matrix\n")    
-    corr_matrix = DataDF.corr()
-    print(corr_matrix)
-    print("")
-
-    # Correlation scatter matrix (not really useful)
-
-    # plt.style.use('dark_background')
-    # fig = plt.figure()
-    # fig.set_size_inches(10,10)
-    # fig.dpi = 300
-    # ax = fig.add_subplot(111)
-    # ax.set_title('Correlation Matrix Scatter Plots')
-
-    # pd.plotting.scatter_matrix(corr_matrix)
-
-    # out_file = "%s/CorrScatter.jpg" % output_path
-    # plt.savefig(out_file)
 
   
 ######################
